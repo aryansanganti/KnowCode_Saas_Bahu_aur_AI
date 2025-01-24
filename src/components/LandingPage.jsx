5fe076c2f7930d6e21eaacdd9bdd3293919f8939
Aryan Sanganti <aryansanganti@gmail.com>
2025-01-24 19:17:26 +0530

    landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>This is the Landing Page
+        link to <a href="/login">Login</a>
+    </div>
+  )
+}
+
+export default LandingPage
